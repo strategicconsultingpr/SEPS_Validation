d96672660f266da8a6171d551c4d963bb9560012
Jose A Ramos <jramos@strategicconsultingpr.com>
2021-08-04 13:06:56 -0400

    dashboard tab

---
+++ SEPS_Prueba/dashboard/Dashboard.aspx.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SEPS.dashboard
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        
+
+    }
+}
